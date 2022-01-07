205023cf2116f435654236847914f3daefd383f5
Ksawery Chodyniecki <ksawery.chodyniecki@gmail.com>
2022-01-07 11:18:38 +0100

    added ping

--- sensor/sensor.cpp
+++ sensor/sensor.cpp
@@ -5,6 +5,7 @@
 #include <queue>
 #include <sstream>
 #include <thread>
+#include <regex>
 
 bool run_program = true;
 std::mutex dataStreamMutex;
@@ -22,6 +23,31 @@ void generateData(std::queue<std::string>* dataStream) {
         if (counter == 20)
             run_program = false;
     }
+}
+
+std::chrono::nanoseconds ping() {
+    char buffer[256];
+    auto file = popen("ping 127.0.0.1 -c 1", "r");
+    fgets(buffer, 256, file);
+    fgets(buffer, 256, file);
+    pclose(file);
+
+    std::smatch result[3];
+    auto temp = std::string(buffer);
+    std::regex_search(temp, result[0], std::regex("time=.*"));
+    temp = std::string(result[0][0]);
+    std::regex_search(temp, result[1], std::regex("[0-9]+\\.[0-9]+"));
+    std::regex_search(temp, result[2], std::regex("[mn]?s"));
+
+    double value;
+    if (result[2][0] == "ms") {
+        value = 1000000 * stod(result[1][0]);
+    } else if(result[2][0] == "s") {
+        value = stod(result[1][0]);
+    } else {
+        throw std::invalid_argument("Ping returned a value in other units than: ms, s!");
+    }
+    return std::chrono::nanoseconds(static_cast<long long>(value));
 }
 
 int main(int argc, char* argv[])
